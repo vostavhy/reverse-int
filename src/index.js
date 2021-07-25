647284b60c5d051d3dfd9b39c6f6debb17ba447a
vostavhy <vostavhy@gmail.com>
2021-07-25 15:09:26 +0300

    feat: write reverse() function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
+  if (n < 0) n *= -1;
   
+  let str_number = n.toString();
+  str_number = str_number.split('').reverse().join('');
+  return Number(str_number);
 }
